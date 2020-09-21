84ce50796fdb33b66daeb37c075351be64e4798c
Drew Ross <mail.drew.ross@gmail.com>
2020-09-21 09:37:23 -0700

    Create Grid component with dynamic size. Change header title.

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Grid from './Grid';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Grid gridSize={500}/>
     </div>
   );
 };
---
+++ app/src/components/Grid.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Grid = props => {
+
+  return (
+    <div className='Grid' style={{'width' : `${props.gridSize}px`, 'height' : `${props.gridSize}px`}}>
+      
+    </div>
+  )
+}
+
+export default Grid;
