c158d77e48c72dc9a4de3ea97db91be733e5de37
Drew Ross <mail.drew.ross@gmail.com>
2020-09-22 15:58:56 -0700

    Update styling and display preset names.

--- app/src/components/Preset.jsx
+++ app/src/components/Preset.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 const Preset = props => {
 
   const { selectPreset } = props;
-  const { image, data } = props.preset;
+  const { image, data, name } = props.preset;
 
   return (
     <div
       className='Preset'
       onClick={() => selectPreset(data)}
     >
+      <p>{name}</p>
       <img src={image} />
     </div>
   );
