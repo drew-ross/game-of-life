d4326bb9d2d6ea40ae0770963a01f09c2295df1a
Drew Ross <mail.drew.ross@gmail.com>
2020-09-22 12:55:42 -0700

    Add functionality to change simulation speed.

--- app/src/components/GameController.jsx
+++ app/src/components/GameController.jsx
@@ -8,10 +8,13 @@ const GameController = props => {
   const [isClearOrStopped, setIsClearOrStopped] = useState(true);
   const [generations, setGenerations] = useState(0);
   const [inputSize, setInputSize] = useState(gridSize);
+  const [speedInterval, setSpeedInterval] = useState(500);
+  const [speedRange, setSpeedRange] = useState(2);
 
   useEffect(() => {
     if (isRunning) {
-      const interval = setInterval(() => step(), 250);
+      console.log(speedInterval);
+      const interval = setInterval(() => step(), speedInterval);
       return () => clearInterval(interval);
     }
   });
@@ -60,18 +63,46 @@ const GameController = props => {
     setGridSize(inputSize);
   };
 
+  const handleRange = e => {
+    if (!isRunning) {
+      setSpeedRange(Number(e.target.value));
+      switch (Number(e.target.value)) {
+        case 1:
+          setSpeedInterval(1000);
+          break;
+        case 2:
+          setSpeedInterval(500);
+          break;
+        case 3:
+          setSpeedInterval(200);
+        default:
+          break;
+      }
+    }
+  };
+
   return (
     <div className='GameController'>
       <div className='buttons'>
         <label>Board Size:
+          <br />
           <input
+            type='number'
             min='4'
             max='30'
             value={inputSize}
-            type='number'
             onChange={handleInput}
           ></input><span>x {inputSize}</span>
         </label><button onClick={handleChangeGridSize}>Set</button><br />
+        <label>Speed:
+          <br />
+          <input
+            type='range'
+            min='1'
+            max='3'
+            value={speedRange}
+            onChange={handleRange}></input>
+        </label><br />
         <button disabled={isRunning} onClick={startGame}>Start</button>
         <button disabled={!isRunning} onClick={pauseGame}>Pause</button>
         <button disabled={isClearOrStopped} onClick={stopGame}>Stop</button>
